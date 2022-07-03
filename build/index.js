4ca4a288e7addda645fa0dd44fdd7f03a8e3cec3
ckt22 <cktangad@connect.ust.hk>
2022-07-03 12:20:17 +0800

    dev dependency - types node@16

--- build/index.js
+++ build/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+// npm install --save-dev @types/node@16 @types/express@4
+app.get('/', function (req, res) {
+    console.log('Hello world!');
+    res.send('Hello World');
+});
+app.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import express from 'express';
+import cluster from 'cluster';
+
+const app = express();
+
+app.use(express.json());
+
+// npm install --save-dev @types/node@16 @types/express@4
+app.get('/', function(req, res) {
+    console.log('Hello world!');
+    res.send('Hello World');
+});
+
+app.listen(3000);
